c674c965ae1c555572dfe32b74ef8237fe291c4f
JunsoMin <minjs4562@gmail.com>
2024-04-04 20:43:11 +0900

    add multivariate gaussian

--- src/reculsive_state_estimation/probabilistic_basic.py
+++ src/reculsive_state_estimation/probabilistic_basic.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+from scipy.integrate import quad
 
 class Gaussian:
     def __init__(self,variance,mean) -> None:
@@ -13,26 +14,30 @@ class Gaussian:
     def get_probabilistic(self,x):
         return 1/(math.sqrt(2*math.pi*self.variance)) * math.exp((-0.5)*(math.pow(x-self.mean,2))/(self.variance))
 
-    def get_probabilistic_integrated(self,x1,x2,dx = 0.1):
-        sum = 0
+    def get_probabilistic_integrated(self, x1, x2):
+        
         if x1 > x2:
             x1, x2 = x2, x1
             print("!!x1,x2 changed!!")
 
-        while x1 != x2:
-            sum += self.get_probabilistic(x1)
-            x1 += dx
+        return quad(lambda x: 1/(math.sqrt(2*math.pi*self.variance)) * math.exp((-0.5)*(math.pow(x-self.mean,2))/(self.variance)),x1,x2)[0]
 
-        return sum
-
-    def get_mutivariate(self,x):
-        '''
-        x in np.array object
-        '''
+class Multivariate_Gaussian_Distribution:
+    def __init__(self,covariance_mat,mean_vector) -> None:
+        self.covariance_matrix = covariance_mat
+        self.mean_vector = mean_vector
+        self.cov_det = np.linalg.det(self.covariance_matrix)
         
-        return
+
+    def get_probablistic(self, x):
+        exp_term = np.exp(-0.5 * np.dot(np.dot((x - self.mean_vector).T, np.linalg.inv(self.covariance_matrix)), (x - self.mean_vector)))
+        return (1 / (np.sqrt((2 * np.pi) ** len(x) * self.cov_det))) * exp_term
+    
+
+    def get_probablistic_integrated(self,x1,x2):
+        integrated = lambda x: self.get_probablistic(x)
+        return quad(integrated,x1,x2)[0]
 
 class Covariance:
     def __init__(self) -> None:
         pass
-
--- test/test.py
+++ test/test.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import numpy as np
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src', 'reculsive_state_estimation'))
 
@@ -7,4 +8,9 @@ import probabilistic_basic
 
 if __name__ == '__main__':
     gaus = probabilistic_basic.Gaussian(100, 10)
-    print(gaus.get_probabilistic_integrated(0, 10,1))
+    print(gaus.get_probabilistic_integrated(0, 0, 10))
+
+    cov_mat = np.array([[2,1],[1,2]])
+    mean_vec = np.array([1,2])
+    multi_gaus = probabilistic_basic.Multivariate_Gaussian_Distribution
+    print(multi_gaus.get_probablistic_integrated())
